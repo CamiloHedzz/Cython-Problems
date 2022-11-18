54f75905a1762820b615b4866aa67a3274a8481a
CamiloHedzz <111331587+CamiloHedzz@users.noreply.github.com>
2022-11-17 20:18:00 -0500

    Delete Algoritmo Burbuja/Ficheros .html directory

--- Algoritmo Burbuja/Ficheros .html/setup.py
+++
@@ -1,6 +0,0 @@
-from distutils.core import setup, Extension
-from Cython.Build import cythonize
-import numpy
-
-exts = (cythonize("burbuja_cy.pyx"))
-setup(include_dirs=[numpy.get_include()], ext_modules = exts)
